67385e18bb86b4c554e5f41c4870ed3f21f10084
Dj <iqdj6205@gmail.com>
2026-09-19 12:35:27 +0200

    Fix palette color import

--- core-ui/src/main/java/com/sere/filemanager/core/ui/AppTheme.kt
+++ core-ui/src/main/java/com/sere/filemanager/core/ui/AppTheme.kt
@@ -4,6 +4,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import com.sere.filemanager.core.model.AppThemeMode
 
 private val DarkColors = darkColorScheme(
